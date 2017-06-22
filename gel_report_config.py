5d0c5a37ebf95b2d6577dbbb4dcd2811510e8050
Aled Jones <aledjones@nhs.net>
2017-06-22 16:51:48 +0100

    add example report and simplify config file

--- gel_report_config.py
+++ gel_report_config.py
@@ -1,10 +1,10 @@
 ####################### Where the App lives##############
-app_home="/home/mokaguys/Apps/CIP_API/"
+app_home="/home/mokaguys/Apps/CIP_API/" # note trailing slash!!
 
 ####################### Authentication ##################
 # path to files containing
-username =  app_home+"auth_username.txt"
-pw = app_home+"auth_pw.txt"
+username =  app_home + "auth_username.txt"
+pw = app_home + "auth_pw.txt"
 
 ####################### Requests module ##################
 #the proxy settings for requests module
